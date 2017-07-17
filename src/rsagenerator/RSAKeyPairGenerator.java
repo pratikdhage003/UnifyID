1fd276d3e79377006dec344b3a6e46a6fa0ae3f3
Pratik Dhage <pratikdhage@Pratiks-MacBook-Pro.local>
2017-07-16 17:54:21 -0700

    updates

--- src/rsagenerator/RSAKeyPairGenerator.java
+++ src/rsagenerator/RSAKeyPairGenerator.java
@@ -11,59 +11,55 @@ public class RSAKeyPairGenerator {
 
 	public static void main(String args[]) throws IOException {
 		/*
-		 * At this point, we are requesting the random org api 30 numbers
-		 * ranging between 20 and 789 using the GET request
+		 * We are requesting the random.org for 20 numbers generated between 18
+		 * and 720 using the http GET request
 		 */
-		URL url = new URL("https://www.random.org/integers/?num=30&min=20&max=789&col=1&base=10&format=plain&rnd=new");
-		HttpURLConnection http = (HttpURLConnection) url.openConnection();
-		http.setRequestMethod("GET");
-		BufferedReader rd = new BufferedReader(new InputStreamReader(http.getInputStream()));
-		String line;
-		int pr = 0;
-		int passcount = 0;
-		int[] pass = new int[3];
+		URL url = new URL("https://www.random.org/integers/?num=20&min=18&max=720&col=1&base=10&format=plain&rnd=new");
+		HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+		httpConnection.setRequestMethod("GET");
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
+		int num = 0;
+		int count = 2;
+		int[] arr = new int[3];
 		/*
 		 * In this section, we read the output that is coming from the GET
 		 * request and check for Prime numbers and then call the RSA Algorithm .
 		 * We also generate the inputs required for running the RSA Algorithm at
 		 * this point.
 		 */
-		while (passcount < 2 && rd.readLine() != null) {
-			line = rd.readLine();
-			pr = Integer.parseInt(line);
-			if (isPrime(pr)) {
-				pass[passcount] = pr;
-				passcount++;
+		while (bufferedReader.readLine() != null && count > 0) {
+			num = Integer.parseInt(bufferedReader.readLine());
+			if (isPrime(num)) {
+				arr[count] = num;
+				count--;
 			}
-
 		}
-		rd.close();
-		int max = (pass[1] - 1) * (pass[0] - 1);
+		bufferedReader.close();
+		int max = (arr[1] - 1) * (arr[0] - 1);
 		int min = 1;
 		max = Math.abs(max);
 		Random r = new Random();
-		int c_e = r.nextInt(max - min) + min;
-		while (c_e <= 0) {
-			c_e = r.nextInt(max - min) + min;
+		int intermediate = r.nextInt(max - min) + min;
+		while (intermediate <= 0) {
+			intermediate = r.nextInt(max - min) + min;
 
 		}
-		pass[2] = c_e;
+		arr[2] = intermediate;
 
-		/*
-		 * 
-		 * 
-		 * */
+		RSAImpl rsaImpl = new RSAImpl();
+		rsaImpl.generateRSA(arr[0], arr[1], arr[2]);
 	}
 
 	/*
 	 * Check if the input coming is a prime number or no
 	 */
-	private static boolean isPrime(int pr) {
-		if (pr % 2 == 0)
+	public static boolean isPrime(int n) {
+		if (n > 2 && (n & 1) == 0)
 			return false;
-		for (int i = 3; i * i <= pr; i += 2) {
-			if (pr % i == 0)
+		for (int i = 2; i <= n / 2; i++) {
+			if (n % i == 0) {
 				return false;
+			}
 		}
 		return true;
 	}
